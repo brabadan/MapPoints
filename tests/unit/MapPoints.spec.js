a7b5f3816b29144f52b37c1f10450889aea94c29
rbakhmudov <rbakhmudov@servicetitan.com>
2019-06-18 23:22:53 +0300

    add tests for methods 'addPoint' and 'removePoint'

--- tests/unit/MapPoints.spec.js
+++ tests/unit/MapPoints.spec.js
@@ -1,23 +1,47 @@
 import {shallowMount} from '@vue/test-utils'
 import MapPoints from '@/components/MapPoints.vue'
 
+const placemark = class {
+    events = {add: jest.fn()};
+    geometry = {_coordinates: [0, 0]};
+};
+
 describe('MapPoints.vue', () => {
-    test('renders newPointName in points list', () => {
+    let wrapper;
+
+    beforeEach((() => {
+        wrapper = shallowMount(MapPoints);
+        // Mocking object 'ymaps'
+        var ymaps = window.ymaps = {
+            Placemark: placemark,
+            Polyline: class {
+            }
+        };
+        // Mocking 'map' object
+        wrapper.setData({map: {getCenter: jest.fn(), geoObjects: {add: jest.fn(), remove: jest.fn()}}});
+    }));
+
+    test('not renders text after initialization', () => {
+        expect(wrapper.text()).toMatch('');
+    });
+
+    test('method addPoint properly add new point and renders newPointName in points list', () => {
         const newPointName = 'Новая точка маршрута';
 
-        const wrapper = shallowMount(MapPoints);
-
-        expect(wrapper.text()).toMatch('');
-
-        wrapper.setData({
-            points: [{
-                placemark: {},
-                name: newPointName,
-                order: 1
-            }]
-        });
+        wrapper.vm.addPoint(newPointName);
 
         expect(wrapper.text()).toMatch(newPointName);
+        expect(wrapper.vm.points.length).toBe(1);
+        expect(wrapper.vm.points[0].name).toBe(newPointName);
 
+    });
+
+    test('method removePoint properly remove element from "points" array ', () => {
+        wrapper.setData({points: [{placemark: new placemark()}, {placemark: new placemark()}]});
+
+        wrapper.vm.removePoint(1);
+
+        expect(wrapper.vm.points.length).toBe(1);
     })
+
 });
