1caa164624ecc278f27a8dfc590e10a6ae6a850b
rbakhmudov <rbakhmudov@servicetitan.com>
2019-06-18 13:54:23 +0300

    attach map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+<!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <Map />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Map from './components/Map';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Map,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div>
+            dfcv
+        </div>
+        <div id="map" style="width: 600px; height: 400px"></div>
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery';  // подключаем jQuery
+    export default {
+        name: "map",
+        methods: {
+            loadJs(url, callback) {
+                $.ajax({
+                    url: url,
+                    dataType: 'script',
+                    success: callback,
+                    async: true
+                });
+            }
+        },
+        mounted() {
+            this.loadJs('https://api-maps.yandex.ru/2.1/?apikey=84058bf3-7dd7-4fb0-a8b6-729d56f01663&lang=ru_RU', function () {
+                ymaps.ready(function(){
+                    var map = new ymaps.Map("map", {
+                        center: [55.76, 37.64],
+                        zoom: 7
+                    });
+                });
+
+            });
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
